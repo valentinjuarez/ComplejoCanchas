2fc95951a17cc9b9f73524e77d29d6d9d91e3a0f
Valentin Juarez <valenjua06@gmail.com>
2026-01-27 03:43:41 -0300

    acomodo el main de vuelta xd

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,8 +22,11 @@ async function bootstrap() {
     credentials: true,
   });
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  console.log('[BOOT] PORT env =', process.env.PORT);
+  console.log('[BOOT] Listening on', port);
+
   await app.listen(port, '0.0.0.0');
-  
-  console.log('Listening on', port);
+
+  console.log('[BOOT] Server started');
 }
 bootstrap();
